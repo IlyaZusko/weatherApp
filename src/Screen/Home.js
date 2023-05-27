059b39c13ff1787eca01e165e16a14e79351daa3
IlyaZusko <IlyaZusko1@gmail.com>
2023-05-27 16:00:28 +0300

    The work of the hourly forecast for the day ahead has been calibrated, the main screen images have been changed depending on the time of day

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -5,13 +5,16 @@ import * as FileSystem from 'expo-file-system';
 import moment from 'moment';
 
 // Constants
-import { FORECAST_ONEDAY_WEATHER_API_URI } from '../Constants/Constants.js';
+import { FORECAST_TWODAYS_WEATHER_API_URI } from '../Constants/Constants.js';
 
 // Navigation
 import { useNavigation } from '@react-navigation/native';
 
 // Images
 import BackgroundImage from '../../assets/img/background.png'
+import DayTimeBackground from '../../assets/img/day_time.png'
+import NightTimeBackground from '../../assets/img/night_time.png'
+import SunriseTimeBackground from '../../assets/img/sunrise_time.png'
 
 // Icons
 import { Feather } from '@expo/vector-icons';
@@ -22,6 +25,8 @@ export default function Home() {
     // States
     const [isReady, setIsReady] = useState(false);
     const [weatherData, setWeatherData] = useState([]);
+
+    const [isNowDayTime, setNowDayTime] = useState(false)
 
     // Local Constants
     const PATH_TO_FILE_WITH_WEATHER_DATA = FileSystem.documentDirectory + 'weatherData.json';
@@ -53,6 +58,25 @@ export default function Home() {
         return unsubscribe;
     }, [NAVIGATION]);
 
+    const timeOfDay = () => {
+        const NOW = moment();
+        const NIGHT_TIME_START = moment('21:00', 'HH:mm');
+        const NIGHT_TIME_END = moment('06:00', 'HH:mm').add(1, 'd');
+        const SUNRISE_TIME_START = moment('06:00', 'HH:mm');
+        const SUNRISE_TIME_END = moment('09:00', 'HH:mm');
+        const DAY_TIME_START = moment('09:00', 'HH:mm');
+        const DAY_TIME_END = moment('21:00', 'HH:mm');
+
+        if (NOW.isBetween(NIGHT_TIME_START, NIGHT_TIME_END)) {
+            return NightTimeBackground;
+        } else if (NOW.isBetween(SUNRISE_TIME_START, SUNRISE_TIME_END)) {
+            return SunriseTimeBackground;
+        } else if (NOW.isBetween(DAY_TIME_START, DAY_TIME_END)) {
+            return DayTimeBackground;
+        }
+        
+    }
+
 
     const date = new Date();
     const dayOfMonth = date.getDate();
@@ -62,9 +86,25 @@ export default function Home() {
     // The function of getting data from the API and writing it to a local file
     const handleWriteFile = async () => {
         try {
-            fetch(`${FORECAST_ONEDAY_WEATHER_API_URI}`)
+            fetch(`${FORECAST_TWODAYS_WEATHER_API_URI}`)
             .then((response) => response.json())
             .then((data) => {
+                const STOP_CYCLE = 2;
+                const FORECASTDAY = data.forecast.forecastday;
+                const TODAY_DAY = 0;
+                const CURRENT_TIME = moment().format('H').replace(/^0+/, '');
+                
+
+                for(let FORECAST_DAY = TODAY_DAY; FORECAST_DAY < STOP_CYCLE; FORECAST_DAY++) {
+                    let filtered = FORECASTDAY[FORECAST_DAY].hour.filter(function (el) {
+                        if(FORECAST_DAY === TODAY_DAY) {
+                            return el.time.split(" ")[1].split(":")[0] >= CURRENT_TIME;
+                        }
+                        return el.time.split(" ")[1].split(":")[0] <= CURRENT_TIME;
+                    });
+                    FORECASTDAY[FORECAST_DAY].hour = filtered;
+                }
+                FORECASTDAY[0].hour = [...FORECASTDAY[0].hour, ...FORECASTDAY[1].hour];
                 FileSystem.writeAsStringAsync(PATH_TO_FILE_WITH_WEATHER_DATA, JSON.stringify(data));
             })
             .catch((error) => {
@@ -83,13 +123,13 @@ export default function Home() {
     }
 
     // Components
-    const WeatherByHourComponent = ({currentTime, isDayOrNight, temperature}) => {
+    const WeatherByHourComponent = ({currentTime, isDayOrNight, temperature, index}) => {
         // Converting a string time value to AM/PM format
         const date = moment(`${currentTime}`, 'YYYY-MM-DD HH:mm').format('h A');
 
         return (
         <View style={style.weatherHoursInfoContainer} >
-            <Text style={style.weatherHoursInfoTime}>{date}</Text>
+            <Text style={style.weatherHoursInfoTime}>{index === 0 ? 'NOW' : date}</Text>
             {isDayOrNight === 0 ? <Feather name="moon" size={24} color="#FFFFFF" /> : <Feather name="sun" size={24} color="#FFFFFF" />}
             <Text style={style.weatherHoursInfoTemp}>{temperature}°C</Text>
         </View>
@@ -108,14 +148,14 @@ export default function Home() {
         <View style={[style.contentWrapper, {paddingTop: CALCULATING_STATUSBAR_HEIGHT}]}>
             <View style={style.wrapper}>
                 <View style={style.imgContainer}>
-                    <ImageBackground source={BackgroundImage} resizeMode='cover' style={style.imgBackground}>
+                    <ImageBackground source={timeOfDay()} resizeMode='cover' style={style.imgBackground}>
                         <View style={[style.imgTitleContainer, {display: 'flex', flexDirection: 'row'}]}>
                             <Text style={[style.imgTitle, {fontFamily: 'pop-sem'}]}>{dayOfMonth} {monthName}, </Text>
                             <Text style={style.imgTitle}>{dayOfWeek}</Text>
                         </View>
                         <View style={[style.imgTitleContainer, {alignItems: 'flex-end', marginBottom: 40}]}>
-                            <Text style={[style.imgTitle, {fontFamily: 'pop-exbold', fontSize: 64}]}>{weatherData && weatherData.current && weatherData.current.temp_c}°C</Text>
-                            <Text style={[style.imgTitle, {fontFamily: 'pop-med', marginTop: -8}]}>Real feel {weatherData.current.feelslike_c}°C</Text>
+                            <Text style={[style.imgTitle, {fontFamily: 'pop-exbold', fontSize: 64, color: '#6E93FF'}]}>{weatherData && weatherData.current && weatherData.current.temp_c}°C</Text>
+                            <Text style={[style.imgTitle, {fontFamily: 'pop-med', marginTop: -8, color: '#6E93FF'}]}>Real feel {weatherData.current.feelslike_c}°C</Text>
                         </View>
                         <View style={[style.imgTitleContainer, {display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}]}>
                             <Text style={[style.imgTitle, {fontFamily: 'pop-med'}]}>{weatherData.location.name}, {weatherData.location.country}</Text>
@@ -143,7 +183,7 @@ export default function Home() {
                 <FlatList
                     data={weatherData.forecast.forecastday[0].hour}
                     renderItem={({item, index}) => 
-                        <WeatherByHourComponent temperature={item.temp_c} isDayOrNight={item.is_day} currentTime={item.time} />
+                        <WeatherByHourComponent temperature={item.temp_c} isDayOrNight={item.is_day} currentTime={item.time} index={index} />
                     }
                     keyExtractor={(_, i) => i.toString()}
                     horizontal
@@ -154,7 +194,6 @@ export default function Home() {
                     ItemSeparatorComponent={Separator}
                 />
             </View>
-            {/* <Text>{weatherData && weatherData.current && weatherData.current.temp_c}</Text> */}
         </View>
     );
 };
